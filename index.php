f151ad36ef2d329f2ebc3da406f941aebeacc2b7
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-10-09 09:16:09 +0600

    Functions and Filtering

--- index.php
+++ index.php
@@ -1,14 +1,58 @@
+<?php
+$books = [
+    [
+        'name' => 'Do Androids of Electric Sheep',
+        'author' => 'Pholip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'http://example.com'
+    ]
+];
+
+function filterByAuthor($books, $author)
+{
+    $filteredBooks = [];
+
+    foreach ($books as $book) {
+        if ($book['author'] === $author) {
+            $filteredBooks[] = $book;
+        }
+    }
+
+    return $filteredBooks;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Demo</title>
 </head>
 
 <body>
-    <h1><?php print "Hello, World" ?></h1>
+    <ul>
+        <?php foreach (filterByAuthor($books, 'Pholip K. Dick') as $book) : ?>
+        <li>
+            <a href="<?= $book['purchaseUrl'] ?>">
+                <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author']; ?>
+            </a>
+        </li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 
 </html>
