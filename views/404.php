849ec2cab5b7423088cceeeaa73cafeb73262d9e
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-11-21 22:51:55 +0600

    PHP For Beginners, Ep 15 - Make a PHP Router

---
+++ views/404.php
@@ -0,0 +1,11 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p class="text-2xl bold mb-2"><?= $code ?> Page not found</p>
+        <a href="/" class="text-xl text-blue underline">Go Back Home</a>
+    </div>
+</main>
+
+<?php require('partials/footer.php') ?>
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,8 +8,8 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <a href="/" class="<?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                        <a href="/about.php" class="<?= urlIs('/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-                        <a href="/contact.php" class="<?= urlIs('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                        <a href="/about" class="<?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                        <a href="/contact" class="<?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
                     </div>
                 </div>
             </div>
