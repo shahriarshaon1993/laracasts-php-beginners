b03c0207be5b30c505c9870ac9ee6bfa6353d766
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-12-05 23:24:55 +0600

    PHP For Beginners, Ep 18 - Extract a PHP Database Class

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 // require 'router.php';
 
-// Contect to our MySql Database
+$db = new Database();
 
-$dsn = "mysql:host=localhost;port=3307;dbname=myapp;user=root;charset=utf8mb4";
+$post = $db->query('select * from posts where id = 1')->fetch(PDO::FETCH_ASSOC);;
 
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare('select * from posts');
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+dd($post);
