23bb49c26e072f9c9e232a5aef8a5b9ebb2d12a2
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-12-06 00:05:08 +0600

    PHP For Beginners, Ep 19 - Environments and Configuration Flexibility

--- index.php
+++ index.php
@@ -10,4 +10,6 @@ $db = new Database($config['database']);
 
 $posts = $db->query('select * from posts')->fetchAll();
 
-dd($posts);
+foreach($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+}
