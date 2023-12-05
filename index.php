b8a5a384be4025b25766142f6f45352ecef073da
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-12-05 23:02:53 +0600

    PHP For Beginners, Ep 17 - PDO First Steps

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// Contect to our MySql Database
+
+$dsn = "mysql:host=localhost;port=3307;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare('select * from posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
