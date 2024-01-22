3faabac845aa4b31c593d2b3fe188f4201d37317
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2024-01-23 01:02:19 +0600

    PHP For Beginners, Ep 23 - Introduction to Authorization

--- controllers/note.php
+++ controllers/note.php
@@ -4,7 +4,18 @@ $config = require('config.php');
 $db = new Database($config['database'], $config['username'], $config['password']);
 
 $heading = "Note";
+$currentUserId = 3;
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+])->fetch();
+
+if(! $note) {
+    abort();
+}
+
+if($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
