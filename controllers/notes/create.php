7ab2d9f15ec5fb4bff6321d206b34abc63ab37bb
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2024-01-24 01:07:37 +0600

    PHP For Beginners, Ep 30 - Autoloading and Extraction

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,15 +1,13 @@
 <?php
 
-require 'Validator.php';
+require base_path('Validator.php');
 
-$config = require 'config.php';
+$config = require base_path('config.php');
 $db = new Database($config['database'], $config['username'], $config['password']);
 
-$heading = "Create Note";
+$errors = [];
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $errors = [];
-
     if (! Validator::string($_POST['body'], 1, 100)) {
         $errors['body'] = 'A body of no more than 100 characters is required.';
     }
@@ -22,4 +20,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-require "views/notes/create.view.php";
+view("notes/create.view.php", [
+    'heading' => 'Create Note',
+    'errors' => $errors
+]);
--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -1,10 +1,11 @@
 <?php
 
-$config = require('config.php');
+$config = require base_path('config.php');
 $db = new Database($config['database'], $config['username'], $config['password']);
-
-$heading = "My Notes";
 
 $notes = $db->query('select * from notes where user_id = 3')->get();
 
-require "views/notes/index.view.php";
+view("notes/index.view.php", [
+    'heading' => 'My Notes',
+    'notes' => $notes
+]);
