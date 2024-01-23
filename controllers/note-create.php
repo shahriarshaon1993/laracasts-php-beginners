54753509f763c71aeac76f37f24623fb01994232
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2024-01-23 22:22:16 +0600

    PHP For Beginners, Ep 26 - Always Escape Untrusted Input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,15 @@
 <?php
 
+$config = require('config.php');
+$db = new Database($config['database'], $config['username'], $config['password']);
+
 $heading = "Create Note";
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST['body']);
+    $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 3
+    ]);
 }
 
 require "views/notes-create.view.php";
