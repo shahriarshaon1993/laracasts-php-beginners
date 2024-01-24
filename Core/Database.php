19ac9fe80d362e8545dc41cb5a052c956f56ae67
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2024-01-24 20:49:39 +0600

    PHP For Beginners, Ep 31 - Namespacing: What, Why, How

--- Core/Database.php
+++ Core/Database.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Core;
 class Database
 {
     public $statement;
@@ -9,8 +10,8 @@ class Database
     {
         $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-        $this->connection = new PDO($dsn, $username, $password, [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        $this->connection = new \PDO($dsn, $username, $password, [
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
         ]);
     }
 
