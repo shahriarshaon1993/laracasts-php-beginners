81aa87f46abb0542a5271b92c5b805bd1a4a29ba
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2024-02-26 11:28:43 +0600

    PHP For Beginners, Ep 48 - Install Two Composer Packages: Collections and PestPHP

---
+++ public/playground.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Collection;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$numbers = new Collection([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+]);
+
+if ($numbers->contains(100)) {
+    die('it contains 10');
+}
+
+//die(var_dump($numbers));
