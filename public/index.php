2f2335dea9145e68b56c37bb132ecc92d7b16a22
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2024-02-26 10:47:08 +0600

    PHP For Beginners, Ep 47 - Composer and Free Autoloading

--- public/index.php
+++ public/index.php
@@ -1,24 +1,17 @@
 <?php
-
-session_start();
-
 use Core\Router;
 use Core\Session;
 use Core\ValidationException;
 
 const BASE_PATH = __DIR__ . '/../';
+require BASE_PATH . 'vendor/autoload.php';
+
+session_start();
 
 require BASE_PATH . 'Core/functions.php';
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', '/', $class);
-
-    require base_path("{$class}.php");
-});
+require base_path('bootstrap.php');
 
 $router = new Router();
-
-require base_path('bootstrap.php');
 $routes = require base_path('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
@@ -32,6 +25,5 @@ try {
 
     return redirect($router->previousUrl());
 }
-
 
 Session::unflash();
